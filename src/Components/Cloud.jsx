3fa75c68d7028058b2610f07c7bc6376346a8a4d
gmendez <mendezgd88@gmail.com>
2023-10-02 15:08:56 -0300

    [

--- src/Components/Cloud.jsx
+++ src/Components/Cloud.jsx
@@ -1,11 +1,32 @@
 import React from "react";
+import Header from "./Header";
+import './Cloud.css';
+import Footer from "./Footer";
 
 const Cloud = () => {
 
     return (
-        <div>
-            <h1>Cloud</h1>
-        </div>
+        <>
+            <Header />
+            <h2 className="subtitulo">Soluciones Cloud</h2>
+            <div className="contenedorCards">
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./aws.jpg" className="card-img-top" alt="checkmk" />
+                    <h5 className="card-title">AWS</h5>
+                    <div className="card-body">
+                        <p className="card-text">Ayudamos a las compañías a realizar una transición exitosa hacia la Nube.</p>
+                    </div>
+                </div>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./ibmcloud.jpg" className="card-img-top" alt="checkmk" />
+                    <h5 className="card-title">IBM Cloud</h5>
+                    <div className="card-body">
+                        <p className="card-text">Nuestros profesionales trabajarán junto a ustedes para que la adopción de cualquier Cloud Solution sea de la manera mas ágil, eficiente y con el menor impacto posible.</p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
--- src/Components/IT.jsx
+++ src/Components/IT.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import "./IT.css";
 
 const IT = () => {
 
     return (
-        <div>
-            <h1>IT</h1>
-        </div>
+        <>
+            <Header />
+            <h2 className="subtitulo">Consultoría estratégica de IT</h2>
+            <div className="contenedorCards">
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./integracion.jpg" className="card-img-top" alt="integracion" />
+                    <h5 className="card-title">Integración de sistemas</h5>
+                    <div className="card-body">
+                        <p className="card-text">Realizamos la integración entre las distintas tecnologías existentes o futuras.</p>
+                    </div>
+                </div>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./servicios.jpg" className="card-img-top" alt="checkmk" />
+                    <h5 className="card-title">Servicios Administrados</h5>
+                    <div className="card-body">
+                        <p className="card-text">Administramos de forma remota o precencial su infraestructura tanto en la Nube como On-premise.</p>
+                    </div>
+                </div>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./gestionproyectos.jpg" className="card-img-top" alt="checkmk" />
+                    <h5 className="card-title">Gestión de proyectos</h5>
+                    <div className="card-body">
+                        <p className="card-text">Explotamos nuestra experiencia y conocimiento para acompañarlos en sus proyectos.</p>
+                    </div>
+                </div>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
+                    <img src="./desarrolloamedida.jpg" className="card-img-top" alt="checkmk" />
+                    <h5 className="card-title">Desarrollo a medida</h5>
+                    <div className="card-body">
+                        <p className="card-text">Realizamos desarrollo de aplicaciones a medida según las necesidades de cada empresa.</p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
