a29f4f4d33564ab3ff9bc75eac816c3f995d02b3
gmendez <mendezgd88@gmail.com>
2023-10-03 01:24:58 -0300

    size

--- src/Components/Monitoreo.jsx
+++ src/Components/Monitoreo.jsx
@@ -8,21 +8,21 @@ const Monitoreo = () => {
             <h2 className="subtitulo">Monitoreo</h2>
             <h3 className="subsub">Nuestro pilar es la plataforma Check_MK, pudiendo configurarse a medida de nuestros clientes.</h3>
             <div className="contenedorCards">
-                <div className="card" style={{ maxWidth: '24rem' }}>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
                     <img src="./checkmk.jpeg" className="card-img-top" alt="checkmk" />
                     <h5 className="card-title">Reporting y KPIs</h5>
                     <div className="card-body">
                         <p className="card-text">Reportes y métricas customizables para visualizaciones en tiempo real.</p>
                     </div>
                 </div>
-                <div className="card" style={{ maxWidth: '24rem' }}>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
                     <img src="./discover.jpg" className="card-img-top" alt="checkmk" />
                     <h5 className="card-title">Discovering</h5>
                     <div className="card-body">
                         <p className="card-text">Descubrimiento, modelado y visualización de la de arquitectura en tiempo real.</p>
                     </div>
                 </div>
-                <div className="card" style={{ maxWidth: '24rem' }}>
+                <div className="card" style={{ maxWidth: '20rem', maxHeight: '30rem' }}>
                     <img src="./gestion.jpg" className="card-img-top" alt="checkmk" />
                     <h5 className="card-title">Gestión de activos</h5>
                     <div className="card-body">
